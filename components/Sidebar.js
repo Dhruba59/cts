1aba54bb71c9c5d746506a066e2be7856c57fc53
khairul alam <khairul.alam@vivacomsolutions.com>
2023-08-07 22:36:55 +0600

    initial

---
+++ components/Sidebar.js
@@ -0,0 +1,126 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import {
+  BsArrowLeftShort,
+  BsSearch,
+  BsChevronDown,
+  BsFileImageFill,
+  BsReverseBackspaceReverse,
+  BsPerson
+} from "react-icons/bs";
+import {
+  AiFillEnvironment,
+  AiOutlineBarChart,
+  AiOutlineMail,
+  AiOutlineSetting,
+  AiOutlineLogout
+} from "react-icons/ai";
+import { RiDashboardFill } from "react-icons/ri";
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const [submenuOpen, setSubmenuOpen] = useState(false);
+  const menus = [
+    { title: "Dashboard" },
+    { title: "Settings", icon: <AiOutlineSetting /> },
+    { title: "Media", spacing: true, icon: <BsFileImageFill /> },
+    {
+      title: "Projects",
+      icon: <AiOutlineBarChart />,
+      submenu: true,
+      subMenuItems: [{ title: "Sub-1" }, { title: "Sub-2" }, { title: "Sub-3" }]
+    },
+    { title: "Profile", icon: <BsPerson /> },
+    { title: "Logout", icon: <AiOutlineLogout /> }
+  ];
+  return (
+    <div
+      className={`bg-dark-purple h-screen p-5 ${
+        open ? "w-72" : "w-20"
+      }  relative duration-300`}
+    >
+      <BsArrowLeftShort
+        className={`bg-white text-dark-purple text-3xl rounded-full absolute 
+        -right-3 top-9 border border-dark-purple ${!open && "rotate-180"}`}
+        onClick={() => setOpen(!open)}
+      />
+      <div className="inline-flex">
+        <AiFillEnvironment
+          className={`bg-amber-300 text-4xl rounded cursor-pointer
+          block float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}
+        />
+        <h1
+          className={`text-white origin-left font-medium text-2xl duration-300 ${
+            !open && "scale-0"
+          }`}
+        >
+          Database
+        </h1>
+      </div>
+      <div
+        className={`flex items-center rounded-md bg-light-white mt-6 ${
+          !open ? "px-2.5" : "px-4"
+        } py-2`}
+      >
+        <BsSearch
+          className={`text-white text block float-left cursor-pointer ${
+            open && "mr-2"
+          }`}
+        />
+        <input
+          type={"Search"}
+          placeholder="Search"
+          className={`text-base bg-transparent w-full text-white focus:outline-none {${
+            !open && "hidden"
+          }}`}
+        ></input>
+      </div>
+      <ul className="pt-2">
+        {menus.map((menu, index) => (
+          <>
+            <li
+              key={index}
+              className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer p-2
+              hover:bg-light-white rounded-md ${
+                menu.spacing ? "mt-9" : "mt-2"
+              }`}
+            >
+              <span className="text-2xl block float-left">
+                {menu.icon ? menu.icon : <RiDashboardFill />}
+              </span>
+              <span
+                className={`text-base font-medium flex-1 duration-200 ${
+                  !open && "hidden"
+                }`}
+              >
+                {menu.title}
+              </span>
+              {menu.submenu && open && (
+                <BsChevronDown
+                  className={`${submenuOpen && "rotate-180"}`}
+                  onClick={() => setSubmenuOpen(!submenuOpen)}
+                />
+              )}
+            </li>
+            {menu.submenu && submenuOpen && open && (
+              <ul>
+                {menu.subMenuItems.map((submenuItem, index) => (
+                  <li
+                    key={index}
+                    className={`text-gray-300 text-sm flex items-center gap-x-4 cursor-pointer
+                                 p-2 px-5 hover:bg-light-white`}
+                  >
+                    {submenuItem.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
