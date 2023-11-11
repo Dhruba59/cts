6ecab7a1d40019e5ea89733f5d77ca71d6ce50bd
khairul <khairultaher@outlook.com>
2023-11-11 13:48:31 +0600

    menu-item-url-added

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"z6g8IN6+czjzVMl7imKqoSsHr3dC/vLBscHXs49+Bzw=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jgidGdlZgrZQv8mwcErsiNbO7gvv3ugXyH2SWe0qH/k=\"}"
