017d398caf17ea071b5156163c0712053793bec0
khairul <khairultaher@outlook.com>
2023-11-11 13:53:33 +0600

    menu-item-url-added

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"jgidGdlZgrZQv8mwcErsiNbO7gvv3ugXyH2SWe0qH/k=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"tvRZhpyRIUMet10WeDqDwQAeXmIJreFpgsib7NHa5FU=\"}"
