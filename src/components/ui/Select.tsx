f2fe340bb85ca92f66dbe4a5a63b6a3d5525602c
Dhruba Jyoti Sarker <dhruba59.sust@gmail.com>
2023-11-29 21:14:24 +0600

    update select component

--- src/components/ui/Select.tsx
+++ src/components/ui/Select.tsx
@@ -2,8 +2,10 @@
 
 import { customStyles } from "@/constants/selectStyle";
 import { SelectProps } from "@/types/common";
-import ReactSelect, { GroupBase } from "react-select";
+import ReactSelect, { GroupBase, GroupProps, OptionProps, Options } from "react-select";
 import Label from "./label";
+import { useEffect, useState } from "react";
+import { SelectOptionType } from "@/model/drop-down-list";
 
 const Select = <
   Option,
@@ -12,7 +14,25 @@ const Select = <
 >(
   props: SelectProps<Option, IsMulti, Group>
 ) => {
-  const { label, placeholder, wrapperClassName, ...restProps } = props;
+  const { label, placeholder, wrapperClassName, options = [], value, ...restProps } = props;
+  const [selected, setSelected] = useState<SelectOptionType[]>([]);
+
+  useEffect(() => {
+    //@ts-ignore
+    setSelected((prevSelected) => {
+      //@ts-ignore
+      if ((Array.isArray(value) && typeof value[0] === 'object') || (value?.label && value?.value)) {
+        return value;
+      } else if (Array.isArray(value)) {
+        let tempValue = value.map(val => val.toString());
+        return options.filter((option: any) => tempValue.includes(option.value));
+      } else if (!Array.isArray(value)) {
+        console.log(value, options);
+        return options.filter((option: any) => option.value === value?.toString())
+      }
+    })
+  }, [value]);
+
   return (
     <div className={wrapperClassName}>
       {label && <Label label={label} className="inline-block mb-2" />}
@@ -22,6 +42,8 @@ const Select = <
           id="long-value-select"
           instanceId="long-value-select"
           placeholder={placeholder ?? "Select"}
+          options={options}
+          value={selected as any}
           {...restProps}
         />
       </div>
