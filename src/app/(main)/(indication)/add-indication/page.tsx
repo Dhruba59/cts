2477f8c7c2ee11262bb4492f47a4ec81ce524720
khairul <khairultaher@outlook.com>
2023-11-30 12:12:43 +0600

    jjj

---
+++ src/app/(main)/(indication)/add-indication/page.tsx
@@ -0,0 +1,10 @@
+import { MainContainer } from "@/components/style-container";
+import AddIndication from "@/features/indication/add";
+
+export default function ChangePassword() {
+  return (
+   <MainContainer>
+      <AddIndication />
+    </MainContainer>
+  );
+}
---
+++ src/app/(main)/(study)/list-of-studies/page.tsx
@@ -0,0 +1,8 @@
+import { MainContainer } from "@/components/style-container";
+import StudyListFeature from "@/features/study/list-of-study";
+
+export default function ChangePassword() {
+  return (
+    <StudyListFeature />
+  );
+}
