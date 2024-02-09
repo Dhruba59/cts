161916a77963dbde9e47ab28249fe8a162ed641e
Dhruba Jyoti Sarker <dhruba59.sust@gmail.com>
2024-02-09 12:30:35 +0600

    update column

--- src/features/change-request/audit/table/columns.tsx
+++ src/features/change-request/audit/table/columns.tsx
@@ -5,6 +5,7 @@ import { IndeterminateCheckbox } from "@/components/ui/checkbox";
 import { Indication, IndicationQuery } from "@/model/indication";
 import { ColumnDef } from "@tanstack/react-table";
 import Cross from "@/components/icons/cross";
+import Warning from "@/components/icons/warning";
 import Link from "next/link";
 import { DeleteOutlined } from "@/assets/icons";
 import { ChangeRequestAuditModel } from "@/model/change-request";
@@ -16,74 +17,96 @@ type ChangeRequestAuditListColumnsProps = {
 }
 export const ChangeRequestAuditListColumns = ({ onViewDetail }: ChangeRequestAuditListColumnsProps)
 : ColumnDef<ChangeRequestAuditModel>[] => {
-return ([
-
+return [
   {
     header: "Modified Date",
     accessorKey: "approvedDate",
     cell: ({ row }) => {
       return (
-        <div className="min-w-[100px]">{formateTableDateTime(row.original.approvedDate)}</div>
+        <div className="min-w-[100px]">
+          {formateTableDateTime(row.original.approvedDate)}
+        </div>
       );
-    }
+    },
   },
   {
     header: "Approver",
     accessorKey: "approver",
     cell: ({ row }) => {
-      return (
-        <div className="min-w-[100px]">{row.original.approver}</div>
-      );
-    }
+      return <div className="min-w-[100px]">{row.original.approver}</div>;
+    },
   },
   {
     header: "Subject ID",
     accessorKey: "subjectId",
     cell: ({ row }) => {
       return <div className=" min-w-[100px]">{row.original.subjectId}</div>;
-    }
+    },
   },
   {
     header: "Site",
     accessorKey: "siteName",
     cell: ({ row }) => {
       return <div className=" min-w-[100px]">{row.original.siteName}</div>;
-    }
+    },
   },
   {
     header: "Study",
     accessorKey: "studyName",
     cell: ({ row }) => {
       return <div className=" min-w-[100px]">{row.original.studyName}</div>;
-    }
+    },
   },
   {
     header: "Protocol",
     accessorKey: "protocolNumber",
     cell: ({ row }) => {
-      return <div className=" min-w-[100px]">{row.original.protocolNumber}</div>;
-    }
-  }, 
+      return (
+        <div className=" min-w-[100px]">{row.original.protocolNumber}</div>
+      );
+    },
+  },
   {
     header: "Subject Name",
     accessorKey: "sponsorSubjectId",
     cell: ({ row }) => {
-      return <div className=" min-w-[100px]">{row.original.sponsorSubjectId}</div>;
-    }
+      return (
+        <div className=" min-w-[100px]">{row.original.sponsorSubjectId}</div>
+      );
+    },
   },
   {
     header: "Subject Initials",
     accessorKey: "firstInit",
     cell: ({ row }) => {
-      return <div className=" min-w-[100px]">{`${row.original.firstInit}${row.original.secondInit}${row.original.thirdInit}`}</div>;
-    }
+      return (
+        <div className=" min-w-[100px]">{`${row.original.firstInit}${row.original.secondInit}${row.original.thirdInit}`}</div>
+      );
+    },
   },
   {
     header: "DOB",
     accessorKey: "dateOfBirth",
     cell: ({ row }) => {
-      return <div className=" min-w-[100px]">{formateTableDate(row.original.dateOfBirth)}</div>;
-    }
+      return (
+        <div className=" min-w-[100px]">
+          {formateTableDate(row.original.dateOfBirth)}
+        </div>
+      );
+    },
+  },
+  {
+    header: "Status",
+    accessorKey: "requestStatus",
+    cell: ({ row }) => {
+      return (
+        <div className="min-w-[50px] text-center">
+          {row.original.requestStatus === "Rejected" && <Cross />}
+          {row.original.requestStatus === "Pending" && <Warning />}
+          {row.original.requestStatus === "Accepted" && <Check />}
+        </div>
+      );
+    },
   },
   {
     id: "actions",
@@ -92,10 +115,15 @@ return ([
     cell: ({ row }) => {
       return (
         <div className="flex items-center gap-6">
-          <View  className="cursor-pointer" onClick={() => onViewDetail(row.original.subjectId, row.original.regionGroupsId)}/>
-           {/* <ChangeRequestAuditDetailModal  subjectId={row.original.subjectId} regionGroupsId={row.original.regionGroupsId}/> */}
+          <View
+            className="cursor-pointer"
+            onClick={() =>
+              onViewDetail(row.original.subjectId, row.original.regionGroupsId)
+            }
+          />
+          {/* <ChangeRequestAuditDetailModal  subjectId={row.original.subjectId} regionGroupsId={row.original.regionGroupsId}/> */}
         </div>
       );
-    }
-  }
-])};
+    },
+  },
+];};
