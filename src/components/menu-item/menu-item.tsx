bcaabe95af95a025fc886571b8bf0b968dab4a55
khairul <khairultaher@outlook.com>
2024-02-12 09:16:22 +0600

    update-to-Link-from-ancor

--- src/components/menu-item/menu-item.tsx
+++ src/components/menu-item/menu-item.tsx
@@ -1,12 +1,13 @@
 import { MenuItemProps } from '@/model/menu-items';
 import React from 'react';
+import Link from 'next/link'
 
 const MenuItem: React.FC<MenuItemProps> = ({ icon, text, url, onClick }) => {
   const content = url ? (
-    <a href={`/${url}`} onClick={onClick} className="flex items-center px-4 py-1 border-b border-gray-100 cursor-pointer transition-colors hover:bg-gray-50 dark:hover:bg-slate-700">
+    <Link href={`/${url}`} onClick={onClick} className="flex items-center px-4 py-1 border-b border-gray-100 cursor-pointer transition-colors hover:bg-gray-50 dark:hover:bg-slate-700">
       {icon && <div className="w-6 h-6 mr-4">{icon}</div>}
       <div>{text}</div>
-    </a>
+    </Link>
   ) : (
     <div onClick={onClick} className="flex items-center px-4 py-1 border-b border-gray-100 cursor-pointer transition-colors hover:bg-gray-50 dark:hover:bg-slate-700">
       {icon && <div className="w-6 h-6 mr-4">{icon}</div>}
