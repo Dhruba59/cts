1f68943863da3926bc64321a75a31e235ec5ec6f
Zubayer Hossain <zubayer.hossain@vivasoftltd.com>
2024-02-22 18:58:54 +0600

    fixed svg tag issue

--- src/features/taining/list/training-list.tsx
+++ src/features/taining/list/training-list.tsx
@@ -58,7 +58,7 @@ const TrainingList = ({ item, trainingId, setTrainingId,selected, setSelected, s
             <div className="flex justify-between items-center font-medium cursor-pointer list-none">
               <div className="text-left">{item.trainingName}</div>
               <div className="text-right transition group-open:rotate-180">
-                <svg fill="none" height="20" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
+                <svg fill="none" height="20" shape-rendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" viewBox="0 0 24 24" width="24"><path d="M6 9l6 6 6-6"></path>
                 </svg>
               </div>
             </div>
