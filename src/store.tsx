1bbc7c1be2d55d0ded43abe4f2cef18db6b1ee1f
Zubayer Hossain <zubayer.hossain@vivasoftltd.com>
2024-02-27 18:52:14 +0600

    added new store values

--- src/store.tsx
+++ src/store.tsx
@@ -1,15 +1,36 @@
 import { create } from 'zustand';
+import { DndDataItem, DndDataType } from './types/common';
 
 type protocolListStore = {
     protocols?: any;
+    initialSiteProtocolIds?: any
+    initialSponsorProtocolIds?: any
+    selectedProtocols: DndDataItem[]
+    dndData?: any;
+    siteDetail?: any;
     addProtocols: (newList: any[]) => void;
+    setProtocols: (newList: number[]) => void;
+    setInitialSiteProtocolIds: (newList: number[]) => void;
+    setSelectedProtocols: (newList: DndDataItem[]) => void;
+    setDndData: (newDndData: DndDataType[]) => void;
+    setSiteDetail: (newSiteDetail: any) => void;
 
 }
 
 const useProtocolListStore = create<protocolListStore>((set) => ({
     protocols: [],
-    addProtocols: (newList) =>
-        set((state) => ({ protocols: newList })),
+    initialSiteProtocolIds: [],
+    initialSponsorProtocolIds: [],
+    selectedProtocols: [],
+    dndData:[],
+    siteDetail: {},
+
+    addProtocols: (newList) => set((state) => ({ protocols: newList })),
+    setProtocols: (newProtocols) => set({ protocols: newProtocols }),
+    setInitialSiteProtocolIds: (newInitialSiteProtocolIds) => set({ initialSiteProtocolIds: newInitialSiteProtocolIds }),
+    setSelectedProtocols: (newSelectedProtocols) => set({ selectedProtocols: newSelectedProtocols }),
+    setDndData: (newDndData) => set({ dndData: [...newDndData] }),
+    setSiteDetail: (newSiteDetail) => set({ siteDetail: newSiteDetail }),
 }));
 
 
