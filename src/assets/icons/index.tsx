7be761c173f6bf4d94e8d2eeb2955fedafc519e3
Zubayer Hossain <zubayer.hossain@vivasoftltd.com>
2024-02-29 19:50:07 +0600

    updates

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -173,7 +173,7 @@ export const CloseIcon = ({ width = "16", height = "16", fill = '#B8B8B8', opaci
 
 
 export const RightArrowIcon = ({ width = "16", height = "16", fill = '#134563', ...rest }: SVGAttributes<SVGElement>) => (
-  <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 64 64" viewBox="0 0 64 64" id="arrow"{...rest}>
+  <svg width={width} height={height} xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 64 64" viewBox="0 0 64 64" id="arrow"{...rest}>
     <g transform="translate(28 328)">
       <path fill={fill} d="M4-272.1c-13.2 0-23.9-10.7-23.9-23.9S-9.2-319.9 4-319.9s23.9 10.7 23.9 23.9S17.2-272.1 4-272.1zm0-45.2c-11.7 0-21.3 9.6-21.3 21.3s9.6 21.3 21.3 21.3 21.3-9.6 21.3-21.3-9.6-21.3-21.3-21.3z"></path>
       <path fill={fill} d="m3.5-282.3-1.8-1.9L13.4-296 1.7-307.8l1.8-1.9L17.2-296 3.5-282.3"></path>
