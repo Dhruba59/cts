ad88c7052deef5b4808c61c2eb917c5e5e778a0c
Dhruba Jyoti Sarker <dhruba59.sust@gmail.com>
2024-03-15 15:16:59 +0600

    update layout style

--- src/components/main/main-body.tsx
+++ src/components/main/main-body.tsx
@@ -6,7 +6,7 @@ const MainBody = ({children}: {
     children: React.ReactNode;}) => {
     const { isSidebarOpen, isSidebarMinimize } = useSidebarContext();
     return (
-        <div className={`mx-auto mt-16 mb-12 ${isSidebarMinimize ? 'w-[95%]' : 'w-[80%]'}`}>
+        <div className={`mx-auto mt-16 mb-12 ${isSidebarMinimize ? 'w-[95%]' : 'w-[95%] md:w-[86%]'}`}>
             {children}
         </div>
     )
