5c955a6a5c68120b1023e651318f5a9dae9a01dd
Khairul Taher <khairultaher@outlook.com>
2024-03-27 15:37:35 +0600

    updated

--- src/components/auth/auth-manager.tsx
+++ src/components/auth/auth-manager.tsx
@@ -1,7 +1,10 @@
-'use client';
-import { useSession } from "next-auth/react";
-import { usePathname, useRouter } from 'next/navigation';
+"use client";
+import { STORAGE_KEY } from "@/constants/storage-constant";
+import { getAccessToken } from "@/utils/helpers";
+import { signOut, useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
+// import { jwtDecode } from "jwt-decode";
 
 const AuthManager = ({ children }: any) => {
   const router = useRouter();
@@ -11,7 +14,6 @@ const AuthManager = ({ children }: any) => {
   useEffect(() => {
     const checkToken = () => {
       if (data) {
-
         // old method to check token
         // const decodedToken = jwtDecode(data?.user?.token?.accessToken);
         // const isValidAccessToken = decodedToken?.exp ? Date.now() < decodedToken.exp * 1000 : false;
@@ -20,25 +22,26 @@ const AuthManager = ({ children }: any) => {
         //   return;
         // }
 
-        if (status === 'authenticated') {
-          // if(!getAccessToken())
-          //   localStorage.setItem(STORAGE_KEY.AUTH_TOKEN, JSON.stringify(data?.user?.token));
+        if (status === "authenticated") {
+          if (!getAccessToken())
+            localStorage.setItem(
+              STORAGE_KEY.AUTH_TOKEN,
+              JSON.stringify(data?.user?.token)
+            );
           if (data?.user?.needToChangePassword) {
-            router.push('/change-password');
-          } else if (pathname.includes('auth')) {
-            router.push('/dashboard');
+            router.push("/change-password");
+          } else if (pathname.includes("auth")) {
+            router.push("/dashboard");
           }
         }
-      }
-      else if (status === 'unauthenticated' && !pathname.includes('auth')) {
-        router.push('/auth/login');
+      } else if (status === "unauthenticated" && !pathname.includes("auth")) {
+        router.push("/auth/login");
       }
     };
     checkToken();
-
   });
 
   return children;
-}
+};
 
 export default AuthManager;
