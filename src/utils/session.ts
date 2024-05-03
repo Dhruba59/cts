4db1ac3b42e76710fece0f6deb0d4d234a7a473d
dhruba59 <dhruba.jyoti@vivasoftltd.com>
2024-05-03 15:02:30 +0600

    update

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,17 +1,27 @@
 import { STORAGE_KEY } from '@/constants/storage-constant';
 import { USER_ROLE_ENUM } from '@/model/enum';
+import { getCookie, hasCookie, setCookie } from 'cookies-next';
+
 export const setRemember = (username: string, password:string, role: USER_ROLE_ENUM) => {
   const data = {
     username,
     password,
     role
   } 
-  localStorage.setItem(STORAGE_KEY.REMEMBER_ME, JSON.stringify(data));
+  // localStorage.setItem(STORAGE_KEY.REMEMBER_ME, JSON.stringify(data));
+  setCookie(STORAGE_KEY.REMEMBER_ME, JSON.stringify(data));
 }
 export const getRememberData = () => {
-  if(localStorage.getItem(STORAGE_KEY.REMEMBER_ME) && localStorage.getItem(STORAGE_KEY.REMEMBER_ME) !== null) {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY.REMEMBER_ME) as string);
+  // const cookieData = cookies().get(STORAGE_KEY.REMEMBER_ME);
+  const cookieData = getCookie(STORAGE_KEY.REMEMBER_ME);
+  const isCookie = hasCookie(STORAGE_KEY.REMEMBER_ME);
+
+  if(isCookie && cookieData) {
+    return JSON.parse(cookieData);
   }
+  // if(localStorage.getItem(STORAGE_KEY.REMEMBER_ME) && localStorage.getItem(STORAGE_KEY.REMEMBER_ME) !== null) {
+  //   return JSON.parse(localStorage.getItem(STORAGE_KEY.REMEMBER_ME) as string);
+  // }
 }
 export const deleteRemember= () => {
   localStorage.removeItem(STORAGE_KEY.REMEMBER_ME);
