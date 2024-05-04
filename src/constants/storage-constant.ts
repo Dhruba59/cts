af15b31fa7a785dfb8129127fc910beea40493e4
Khairul Taher <khairultaher@outlook.com>
2024-05-04 18:30:20 +0600

    remember data added to cookies with encryption

--- src/constants/storage-constant.ts
+++ src/constants/storage-constant.ts
@@ -3,6 +3,6 @@ export const STORAGE_KEY = {
   ACCESS_TOKEN: 'accessToken',
   REFRESH_TOKEN: 'refreshToken',
   AUTH_TOKEN: 'authToken',
-  REMEMBER_ME: 'next-auth.session-re',
-  ROLE: 'next-auth.session-ro'
+  REMEMBER_ME: '__Secure-next-auth.session-re',
+  ROLE: '__Secure-next-auth.session-ro'
 }
